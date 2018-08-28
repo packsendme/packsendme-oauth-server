e9c0c04d872d84625bc248375606c597fc9a107d
packsendme <packsendme@gmail.com>
2018-08-28 13:37:38 +0200

    new microservice oauth

---
+++ src/main/java/com/packsendme/microservice/oauth/PacksendmeOauthServerApplication.java
@@ -0,0 +1,12 @@
+package com.packsendme.microservice.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacksendmeOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacksendmeOauthServerApplication.class, args);
+	}
+}
